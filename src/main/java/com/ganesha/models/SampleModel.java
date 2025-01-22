1bfb94ca42d7d5b0eb82c03d80c6ca439821085f
MJ Schanne <mjschanne@microsoft.com>
2025-01-21 23:24:12 -0500

    Add sample model and sample service

---
+++ src/main/java/com/ganesha/models/SampleModel.java
@@ -0,0 +1,10 @@
+package com.ganesha.models;
+
+import java.util.List;
+import java.util.UUID;
+
+public class SampleModel {
+    public UUID id;
+    public String name;
+    public List<String> tags;
+}
