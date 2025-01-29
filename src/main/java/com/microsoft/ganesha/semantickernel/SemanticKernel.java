60ee202e9e6123bb097677f6121bc480b4c0f701
Camille Jetzer <cajetzer@microsoft.com>
2025-01-29 09:59:08 -0500

    new predictReason endpoint, removed lightstatus endpoint

--- src/main/java/com/microsoft/ganesha/semantickernel/SemanticKernel.java
+++ src/main/java/com/microsoft/ganesha/semantickernel/SemanticKernel.java
@@ -5,8 +5,6 @@ import java.util.List;
 import com.azure.ai.openai.OpenAIAsyncClient;
 import com.azure.ai.openai.OpenAIClientBuilder;
 import com.azure.core.credential.TokenCredential;
-import com.azure.core.http.HttpPipelineBuilder;
-import com.azure.core.http.policy.HttpPipelinePolicy;
 import com.azure.identity.ClientSecretCredentialBuilder;
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.google.gson.Gson;
@@ -66,6 +64,44 @@ public class SemanticKernel {
         return "";
     }    
 
+    public String GetReasons(String memberid) throws SemanticKernelException, ServiceNotFoundException {
+       
+        Kernel kernel = InstantiateKernel();
+
+        String prompt = """
+        You are an assistant to customer service agents for a prescription fulfillment call center. Given a provided set of activities associated with a specific member, predict the reason they would call into the call center. Include all possible reasons for a call, ordered by likelihood if the percentage of call reasons are the following:
+        Cancelled orders - 50%
+        Closed orders - 21%
+        Booked orders - 19%
+        Entered orders - 10%
+        ##Example##
+        Likely call reason in order of highest likelihood:
+        - [medication name] from date [prescription date] which was canceled
+        - [medication name] from date [prescription date] in Entered state
+        """;
+        
+        // Enable planning
+        InvocationContext invocationContext = new Builder()
+            .withReturnMode(InvocationReturnMode.LAST_MESSAGE_ONLY)
+            .withToolCallBehavior(ToolCallBehavior.allowAllKernelFunctions(true))
+            .withContextVariableConverter(ContextVariableTypeConverter.builder(OrderActivities.class)
+                .toPromptString(new Gson()::toJson)
+                .build())
+            .build();
+
+        List<ChatMessageContent<?>> results;
+        ChatCompletionService chatCompletionService = kernel.getService(
+            ChatCompletionService.class);
+        try {
+            results = chatCompletionService.getChatMessageContentsAsync(
+                prompt, kernel, invocationContext).block();
+            return results.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }    
+
     private Kernel InstantiateKernel() throws SemanticKernelException{
         TokenCredential credential = null;
         if(config.getAZURE_CLIENT_ID() != null && !config.getAZURE_CLIENT_ID().isEmpty()) {
@@ -85,16 +121,6 @@ public class SemanticKernel {
         }   
 
         OpenAIAsyncClient client;
-
-        //        HttpPipelinePolicy customHeaderPolicy = (context, next) -> {
-        //     context.getHttpRequest().getHeaders().set("projectId", projectId);
-        //     return next.process();
-        // };
-
-        // HttpPipelineBuilder pipelineBuilder = new HttpPipelineBuilder()
-        //     .policies(customHeaderPolicy);
-
-        // var pipeline = pipelineBuilder.build();
 
         client = new OpenAIClientBuilder()
             .credential(credential)
