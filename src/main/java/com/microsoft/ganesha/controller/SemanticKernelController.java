0329af41371968255d228d7911b9ed836ab7c012
MJ Schanne <mjschanne@microsoft.com>
2025-01-31 01:43:48 -0500

    Add conversation endpoint

--- src/main/java/com/microsoft/ganesha/controller/SemanticKernelController.java
+++ src/main/java/com/microsoft/ganesha/controller/SemanticKernelController.java
@@ -1,18 +1,19 @@
 package com.microsoft.ganesha.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.microsoft.ganesha.config.AppConfig;
+import org.springframework.web.server.ResponseStatusException;
 import com.microsoft.ganesha.exception.SemanticKernelException;
+import com.microsoft.ganesha.interfaces.MongoService;
+import com.microsoft.ganesha.models.Conversation;
+import com.microsoft.ganesha.models.DisplayChatMessage;
 import com.microsoft.ganesha.semantickernel.MemberId;
 import com.microsoft.ganesha.semantickernel.Prompt;
 import com.microsoft.ganesha.semantickernel.SemanticKernel;
 import com.microsoft.semantickernel.services.ServiceNotFoundException;
- 
+import com.microsoft.semantickernel.services.chatcompletion.AuthorRole;
 
 @RestController
 public class SemanticKernelController {
@@ -23,7 +24,7 @@ public class SemanticKernelController {
 
     private SemanticKernel _kernel;
     private MongoService _mongoService;
-    
+
     @PostMapping("/prompt")
     String replaceEmployee(@RequestBody Prompt prompt) throws SemanticKernelException, ServiceNotFoundException {
         return _kernel.GetSKResult(prompt.getPrompt());
@@ -33,8 +34,65 @@ public class SemanticKernelController {
     String predictReason(@RequestBody MemberId memberid) throws SemanticKernelException, ServiceNotFoundException {
         return _kernel.GetReasons(memberid.GetMemberId());
     }
-    
-        SemanticKernel sk = new SemanticKernel(config);
-        return sk.GetReasons(memberid.GetMemberId());
+
+    @PostMapping("/conversation")
+    Conversation conversation(@RequestBody Conversation conversation)
+            throws SemanticKernelException, ServiceNotFoundException {
+        try {
+            var conversationId = conversation.getId();
+            var messages = conversation.getMessages();
+
+            if (messages == null || messages.isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Conversation messages cannot be empty");
+            }
+
+            var conversationDoesNotExist = true;
+
+            if (conversationId == null) {
+                conversationDoesNotExist = true;
+            } else {
+                try {
+                    var dbConversation = _mongoService.GetConversation(conversationId);
+
+                    conversationDoesNotExist = dbConversation == null;
+                } catch (Exception e) {
+                    conversationDoesNotExist = true;
+                }
+            }
+
+            if (conversationDoesNotExist) {
+                conversation.setId(java.util.UUID.randomUUID());
+            }
+
+            // add latest message in conversation to db or if convo doesn't exist, make it
+            // so
+            _mongoService.UpsertConversation(conversation);
+
+            if (conversationDoesNotExist) {
+                String reason = _kernel.GetReasons(messages.get(0).getMessage());
+
+                if (reason != null) {
+                    var reasonMessage = new DisplayChatMessage(reason, AuthorRole.ASSISTANT.toString(),
+                            java.time.OffsetDateTime.now());
+
+                    conversation.getMessages().add(reasonMessage);
+                }
+            } else {
+                var chatHistory = _kernel.Converse(conversation.toChatHistory());
+
+                // right now this constructor filters out tool and system messages
+                // we may want to consider if we want to persist them for transparency?
+                conversation = new Conversation(chatHistory);
+            }
+
+            // now make sure that completion is persisted
+            _mongoService.UpsertConversation(conversation);
+
+            return conversation;
+        } catch (Exception e) {
+            // todo: refine exceptions and handling throughout stack...
+            // probably mongo service interface needs closer look
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
     }
-  }
+}
